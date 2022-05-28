d8136c85fbb54f29c5bce5c5d74345b8c1724852
Rifat <hossen.mdrifat20@gmail.com>
2022-05-28 19:03:34 +0600

    solved is your hoursehoe on the other hoof

---
+++ Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int arr[4],counter=0;
+    for(int i=0;i<4;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+4);
+    for(int i=0; i<3; i++)
+    {
+        if(arr[i]==arr[i+1])
+        {
+           counter++;
+        }
+    }
+    cout<<counter;
+}
